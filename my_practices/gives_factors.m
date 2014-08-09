a60ff42f75c88cf913e756f5d8c8d01765dda162
Ashutosh Purushottam <sonu.puru1807@gmail.com>
2014-08-09 12:45:05 +0530

    suggesting an alternative implementation

--- my_practices/gives_factors.m
+++ my_practices/gives_factors.m
@@ -6,3 +6,14 @@ funtion y = gives_factors (x)
 		if mod(x, i) == 0,
 			y = [y, [i]];
 	end	
+
+%added by ashu
+%this function computes the vector containing fibonacci terms
+function y = give_factors(x)
+	j = 1
+	for i = 1:x,
+		if mod(x, i) == 0,
+			y(j) = i;
+			j = j + 1;
+		end
+	end				
