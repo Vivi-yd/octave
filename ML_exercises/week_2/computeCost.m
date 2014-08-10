bd7c42aca8807be451c6b4778a5cbcf3605ddb47
Vivi YD <vivid321@yahoo.com>
2014-08-10 14:26:24 +1000

    corrected and improved code

--- ML_exercises/week_2/computeCost.m
+++ ML_exercises/week_2/computeCost.m
@@ -1,5 +1,4 @@
 function J = computeCost(X, y, theta)
-
 %COMPUTECOST Compute cost for linear regression
 %   J = COMPUTECOST(X, y, theta) computes the cost of using theta as the
 %   parameter for linear regression to fit the data points in X and y
@@ -15,11 +14,11 @@ J = 0;
 %               You should set J to the cost.
 
 % calculate the prediction of each element in given vector X by the hypothesis.
-   for i=1:m,
-       x = [1; X(i, 2)];
-       J = J + (theta' * x  - y(i))^2;
-   end
-   
+
+	for i=1:m,
+		%x = [1; X(i, 2)];
+		J = J + (theta' * X(i, :)'  - y(i))^2;
+	end
 J = 1/(2*m) * J; 
 
 
