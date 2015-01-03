e88105463acb144e3fcfc227f5969fca9b0817f0
Vivi YD <vivid321@yahoo.com>
2015-01-04 00:50:33 +1100

    added vectorized version

--- ML_exercises/week_3/costFunctionReg.m
+++ ML_exercises/week_3/costFunctionReg.m
@@ -94,3 +94,42 @@ for j = 2:n,
 % =============================================================
 
 end
+
+
+
+% ================================ vectorized version ==========================
+
+function [J, grad] = costFunctionReg(theta, X, y, lambda)
+%COSTFUNCTIONREG Compute cost and gradient for logistic regression with regularization
+%   J = COSTFUNCTIONREG(theta, X, y, lambda) computes the cost of using
+%   theta as the parameter for regularized logistic regression and the
+%   gradient of the cost w.r.t. to the parameters. 
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+% You need to return the following variables correctly 
+J = 0;
+grad = zeros(size(theta));
+
+
+%====================== Compute Cost ===================
+
+%define the hypothesis h(X) for classification (vectorized)
+hypothesis = sigmoid(X*theta);
+
+J = (1/m)*sum((-y' * log(hypothesis) - (1-y)' * log(1-hypothesis))) + (lambda/(2*m))* sum(theta(2:end).^2);
+
+
+%======================= Gradient (vectorized) ======================
+
+% the theta(1) (or theta zero) part
+grad(1) = 1/m * (X(:,1)' * (hypothesis - y));
+
+%%%% regularization part starting from theta(2)
+grad(2:end) = (1/m * (X(:,2:end)' * (hypothesis - y))) + (lambda/m)*theta(2:end);
+
+% =============================================================
+
+
+end
